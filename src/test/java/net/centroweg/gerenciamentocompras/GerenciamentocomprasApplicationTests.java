c6d9b58c7326eb9175a8f6378bf797f5d42ca2b3
HUGO DELEON GIMINIANI DE SOUZA PAIM <hugo_paim@estudante.sesisenai.org.br>
2026-05-14 15:12:02 -0300

    initial commit

---
+++ src/test/java/net/centroweg/gerenciamentocompras/GerenciamentocomprasApplicationTests.java
@@ -0,0 +1,13 @@
+package net.centroweg.gerenciamentocompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentocomprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

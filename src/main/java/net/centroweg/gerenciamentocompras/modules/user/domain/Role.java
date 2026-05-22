7c42e0090ac096f508fa2d3cd5504066727c3015
MARIA EDUARDA ZABEL <maria_e_zabel@estudante.sesisenai.org.br>
2026-05-22 16:10:39 -0300

    API-14 feat/implementa classe de usuário e role com interface de nível de acesso

---
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/user/domain/Role.java
@@ -0,0 +1,35 @@
+package net.centroweg.gerenciamentocompras.modules.user.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.centroweg.gerenciamentocompras.modules.user.domain.rolelevels.RoleLevels;
+
+import java.util.List;
+
+@Entity
+@Table(name = "role")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role implements RoleLevels {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Override
+    public String getRole(){
+        return this.name;
+    }
+
+    @OneToMany(mappedBy = "role")
+    private List<User> usuarios;
+
+}
---
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/user/domain/User.java
@@ -0,0 +1,47 @@
+package net.centroweg.gerenciamentocompras.modules.user.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String extensionNumber;
+
+    @Column(nullable = false)
+    private boolean active;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    private Role role;
+}

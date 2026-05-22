f5b4caf700e8abb9b19e8b5741e2d66304c4576a
EMANUELLE CRISTINA HOSTIN <emanuelle_hostin@estudante.sesisenai.org.br>
2026-05-22 20:10:43 -0300

    [API-45] feat/Implementa mapper de status

---
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/request/service/mapper/StatusMapperImpl.java
@@ -0,0 +1,26 @@
+package net.centroweg.gerenciamentocompras.modules.request.service.mapper;
+
+import lombok.RequiredArgsConstructor;
+import net.centroweg.gerenciamentocompras.modules.request.domain.Status;
+import net.centroweg.gerenciamentocompras.modules.request.presentation.dto.request.StatusRequest;
+import net.centroweg.gerenciamentocompras.modules.request.presentation.dto.response.StatusResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StatusMapperImpl implements IStatusMapper {
+
+    public Status toEntity(StatusRequest statusRequest) {
+        return new Status(
+                statusRequest.name(),
+                statusRequest.description()
+        );
+    }
+
+    public StatusResponse toResponse(Status status) {
+        return new StatusResponse(
+                status.getId(),
+                status.getName(),
+                status.getDescription()
+        );
+    }
+}

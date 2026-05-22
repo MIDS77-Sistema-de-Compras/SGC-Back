f534d76ffce4c0741bedc7ec4482d45a350be216
EMANUELLE CRISTINA HOSTIN <emanuelle_hostin@estudante.sesisenai.org.br>
2026-05-22 20:08:33 -0300

    [API-46] feat/Implementa repositório de status

---
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/request/infrastructure/persistence/StatusRepository.java
@@ -0,0 +1,9 @@
+package net.centroweg.gerenciamentocompras.modules.request.infrastructure.persistence;
+
+import net.centroweg.gerenciamentocompras.modules.request.domain.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StatusRepository extends JpaRepository<Status, Long> {
+}

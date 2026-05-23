d8b6ba02ddb300b45df979d403286f9317893b92
HUGO DELEON GIMINIANI DE SOUZA PAIM <hugo_paim@estudante.sesisenai.org.br>
2026-05-22 21:53:48 -0300

    API-[22] chore/configura inicialmente o Security Filter Chain

---
+++ src/main/java/net/centroweg/gerenciamentocompras/config/security/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package net.centroweg.gerenciamentocompras.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .authorizeHttpRequests(authorizeRequests -> {
+                    authorizeRequests.anyRequest().permitAll();
+                })
+                .build();
+    }
+}

c7ff07c0fbf37f4fc3d3a062f6ff69232f3a3f04
Gabriel Ehrat <gabriel_e_fagundes@estudante.sesisenai.org.br>
2026-05-25 19:48:15 -0300

    feat/implement basic CRUD operations and exception handling for provision #36 #37 #38 #39 (#51)
    * API-36 feat/Implementando a base do modulo provision
    
    * [API-37] feat: Add feature de buscar provisions (todas e por ID)
    
    * [API-38] feat: Add feature de atualizar provisions
    
    * [API-39] feat: Add feature de deletar e packages restantes
    
    * [API-07] feat: add global exception handler (#44)
    
    * API-08 feat/inicia a estrutura de pastas do projeto
    
    * API-08 feat/termina-arquitetura-principal
    
    * [API-07] feat/add-global-exception-handler
    
    ---------
    
    Co-authored-by: LEANDRO FILIPY DE LIMA <leandro_f_lima@estudante.sesisenai.org.br>
    
    * [API-07] fix: move exception classes to correct package (#50)
    
    * [API-07] fix/corrige-package-de-exceptions
    
    * [API-07] fix/remove-pastas-vazias
    
    * [API-39] feat: Add testes de integração para modulo provision
    
    * [API-63] fix: Ajustando linguagem e exception nos testes
    
    * [API-63] fix: Ajustando exceção para corresponder com GlobalExceptionHandler
    
    * [API-63] fix: Add exception no GlobalHandler e remove .history
    
    ---------
    
    Co-authored-by: Gabi Vitória <gabrielli_glowatski@estudante.sesisenai.org.br>
    Co-authored-by: LEANDRO FILIPY DE LIMA <leandro_f_lima@estudante.sesisenai.org.br>
    Co-authored-by: Hugo Deleon <hugo_paim@estudante.sesisenai.org.br>

---
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/provision/presentation/dto/request/ProvisionRequest.java
@@ -0,0 +1,16 @@
+package net.centroweg.gerenciamentocompras.modules.provision.presentation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ProvisionRequest(
+    @NotBlank(message="O nome do serviço não pode estar vazio.")
+    @NotNull(message="O nome do serviço não pode ser nulo.")
+    String name,
+
+    @NotNull(message="O valor total do serviço não pode ser nulo.")
+    Double totalValue,
+
+    @NotBlank(message="A descrição do serviço não pode estar vazia.")
+    String description
+) {}

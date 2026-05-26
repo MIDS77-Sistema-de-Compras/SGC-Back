74d7726f4aa95b565c823de5787782c0b69fa7fc
GABRIELLI VITÓRIA GLOWATSKI <gabrielli_glowatski@estudante.sesisenai.org.br>
2026-05-25 21:12:11 -0300

    [API-72] feature/adiciona-repository-no-CrBranch

---
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/cr/infrastructure/persistence/CrBranchRepository.java
@@ -0,0 +1,15 @@
+package net.centroweg.gerenciamentocompras.modules.cr.infrastructure.persistence;
+
+import net.centroweg.gerenciamentocompras.modules.cr.domain.CrBranch;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CrBranchRepository extends JpaRepository<CrBranch, Long> {
+    List<CrBranch> findByBranchId(Long branchId);
+    Optional<CrBranch> findByCrIdAndBranchId(Long crId, Long branchId);
+    Optional<CrBranch> findByIdAndResponsibleUserIdIsNotNull(Long id);
+}

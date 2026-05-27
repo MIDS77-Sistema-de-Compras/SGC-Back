9c01b6ef8083e55addffdeb3e75b7c63836b2d14
Gabrielli Glowatski <gabrielli_glowatski@estudante.sesisenai.org.br>
2026-05-27 19:26:16 -0300

    [API-68] feat/adiciona-entidade-solicitacao

---
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/request/domain/Request.java
@@ -0,0 +1,48 @@
+package net.centroweg.gerenciamentocompras.modules.request.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.centroweg.gerenciamentocompras.modules.cr.domain.CrBranch;
+import net.centroweg.gerenciamentocompras.modules.user.domain.entity.User;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "requests")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Request {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime requestDate;
+
+    @ManyToOne
+    @JoinColumn(name = "cr_branch_id", nullable = false)
+    private CrBranch crBranch;
+
+    @ManyToOne
+    @JoinColumn(name = "status_id", nullable = false)
+    private Status status;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @ManyToMany
+    @JoinTable(
+            name = "request_users",
+            joinColumns = @JoinColumn(name = "request_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    List<User> createdByUsers = new ArrayList<>();
+}

e7f49a0e8baa5b64c5c6017c79806dbd404df393
andrMiotto <andre_lm_pereira@estudante.sesisenai.org.br>
2026-05-29 20:12:29 -0300

    [BACK-125] docs: implementa JavaDoc para request

--- src/main/java/net/centroweg/gerenciamentocompras/modules/request/presentation/dto/response/StatusResponse.java
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/request/presentation/dto/response/StatusResponse.java
@@ -1,5 +1,15 @@
 package net.centroweg.gerenciamentocompras.modules.request.presentation.dto.response;
-
+/**
+ * DTO responsável por representar os dados
+ * retornados nas respostas da entidade Status.
+ *
+ * @param id identificador do status
+ * @param name nome do status
+ * @param description descrição do status
+ *
+ * @author André
+ * @since 1.0
+ */
 public record StatusResponse(
 
         Long id,
--- src/main/java/net/centroweg/gerenciamentocompras/modules/request/service/mapper/IStatusMapper.java
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/request/service/mapper/IStatusMapper.java
@@ -4,11 +4,32 @@ import lombok.RequiredArgsConstructor;
 import net.centroweg.gerenciamentocompras.modules.request.domain.Status;
 import net.centroweg.gerenciamentocompras.modules.request.presentation.dto.request.StatusRequest;
 import net.centroweg.gerenciamentocompras.modules.request.presentation.dto.response.StatusResponse;
-
+/**
+ * Interface responsável pelo mapeamento entre
+ * entidade, DTO de requisição e DTO de resposta
+ * da entidade Status.
+ *
+ * <p>Define os métodos de conversão utilizados
+ * na comunicação entre as camadas da aplicação.</p>
+ *
+ * @author André
+ * @since 1.0
+ */
 public interface IStatusMapper {
-
+    /**
+     * Converte um DTO de requisição em entidade.
+     *
+     * @param statusRequest dados recebidos na requisição
+     * @return entidade Status convertida
+     */
     public Status toEntity (StatusRequest statusRequest);
 
+    /**
+     * Converte uma entidade em DTO de resposta.
+     *
+     * @param status entidade Status
+     * @return DTO de resposta convertido
+     */
     public StatusResponse toResponse (Status status);
 
 }

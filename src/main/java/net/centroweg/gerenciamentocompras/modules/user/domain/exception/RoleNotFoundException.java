52b7bd83f27a8f94f1dc2d8bbb4d3e062e677460
LEANDRO FILIPY DE LIMA <leandro_f_lima@estudante.sesisenai.org.br>
2026-05-29 18:02:57 -0300

    API-124 fix/ajusta-regras-de-negocio-de-usuário

--- src/main/java/net/centroweg/gerenciamentocompras/modules/user/domain/exception/RoleNotFoundException.java
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/user/domain/exception/RoleNotFoundException.java
@@ -4,7 +4,7 @@ import net.centroweg.gerenciamentocompras.shared.exception.BusinessException;
 import org.springframework.http.HttpStatus;
 
 public class RoleNotFoundException extends BusinessException {
-    public RoleNotFoundException(Long id) {
-        super("Role não encontrada com id: " + id, HttpStatus.NOT_FOUND);
+    public RoleNotFoundException() {
+        super("Role não encontrada ", HttpStatus.NOT_FOUND);
     }
 }

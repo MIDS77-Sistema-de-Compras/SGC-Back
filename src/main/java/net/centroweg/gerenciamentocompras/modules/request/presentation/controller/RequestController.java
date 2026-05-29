46d871f07858b22693a4ff7a4116db2838c533f7
MARIA EDUARDA ZABEL <maria_e_zabel@estudante.sesisenai.org.br>
2026-05-29 14:35:44 -0300

    API-118 feature/implementa controller no CRUD de solicitação

---
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/request/presentation/controller/RequestController.java
@@ -0,0 +1,46 @@
+package net.centroweg.gerenciamentocompras.modules.request.presentation.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import net.centroweg.gerenciamentocompras.modules.request.presentation.dto.request.CreateRequestRequest;
+import net.centroweg.gerenciamentocompras.modules.request.presentation.dto.response.RequestResponse;
+import net.centroweg.gerenciamentocompras.modules.request.service.useCases.serviceIntrf.RequestService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/requests")
+@RestController
+public class RequestController {
+
+    private final RequestService requestService;
+
+    @PostMapping
+    public ResponseEntity<RequestResponse> createRequest(@Valid @RequestBody CreateRequestRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(requestService.createRequest(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RequestResponse>> findAllRequest(){
+        return ResponseEntity.ok(requestService.findAllRequest());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RequestResponse> findRequestById(@PathVariable Long id){
+        return ResponseEntity.ok(requestService.findRequestById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<RequestResponse> updateRequest(@Valid @RequestBody CreateRequestRequest request, @PathVariable Long id){
+        return ResponseEntity.ok(requestService.updateRequest(request, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteRequest(@PathVariable Long id){
+        requestService.deleteRequest(id);
+        return ResponseEntity.noContent().build();
+    }
+}

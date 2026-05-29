370dcebf83a23ac447e2cb0b7975ea818e148588
HUGO DELEON GIMINIANI DE SOUZA PAIM <hugo_paim@estudante.sesisenai.org.br>
2026-05-29 14:08:06 -0300

    API-108 feat: implementa service e controller de autenticação

--- src/main/java/net/centroweg/gerenciamentocompras/modules/auth/service/CustomUserDetailsService.java
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/auth/service/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package net.centroweg.gerenciamentocompras.modules.auth.service;
 
 import lombok.RequiredArgsConstructor;
 import net.centroweg.gerenciamentocompras.modules.auth.domain.UserPrincipal;
+import net.centroweg.gerenciamentocompras.modules.auth.service.api.AuthPublicApi;
 import net.centroweg.gerenciamentocompras.modules.user.domain.entity.User;
 import net.centroweg.gerenciamentocompras.modules.user.infrastructure.persistence.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,8 +14,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final UserRepository userRepository;
-    private final UserPrincipal userPrincipal;
+    private final AuthPublicApi authPublicApi;
 
     @Override
     public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
@@ -22,7 +22,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         String cleanLogin = login.trim();
         String cleanCpf = cleanLogin.replaceAll("\\D", "");
 
-        User userSearched = userRepository.findByEmailOrCpf(cleanLogin, cleanCpf)
+        User userSearched = authPublicApi.findByEmailOrCpf(cleanLogin, cleanCpf)
                 .orElseThrow( () -> new UsernameNotFoundException("Credenciais inválidas para o login fornecido"));
 
         return new UserPrincipal(userSearched);

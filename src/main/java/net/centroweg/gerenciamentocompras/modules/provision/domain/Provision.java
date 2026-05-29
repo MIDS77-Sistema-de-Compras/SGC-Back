7fba663cdf6712c1ea44435934d5232a84a739a4
gabrielEFagundes <gabriel_e_fagundes@estudante.sesisenai.org.br>
2026-05-29 20:38:29 -0300

    [API-125] docs: documenta modulo provision

--- src/main/java/net/centroweg/gerenciamentocompras/modules/provision/domain/Provision.java
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/provision/domain/Provision.java
@@ -12,6 +12,13 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Classe representante do módulo serviço, mapeada diretamente com o banco de dados.
+ * <p>
+ * Detém dados para persistencia.
+ * @author gabrielEFagundes
+ * @version 0.1.0
+ */
 @Entity
 @Table(name="provision")
 @AllArgsConstructor
--- src/main/java/net/centroweg/gerenciamentocompras/modules/provision/service/GetProvisionService.java
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/provision/service/GetProvisionService.java
@@ -10,6 +10,13 @@ import net.centroweg.gerenciamentocompras.modules.provision.infrastructure.persi
 import net.centroweg.gerenciamentocompras.modules.provision.presentation.dto.response.ProvisionResponse;
 import net.centroweg.gerenciamentocompras.modules.provision.service.mapper.ProvisionMapper;
 
+/**
+ * Classe responsável por buscar os serviços no banco de dados.
+ * @author gabrielEFagundes
+ * @version 0.1.0
+ * @see ProvisionRepository
+ * @see ProvisionMapper
+ */
 @Service
 @RequiredArgsConstructor
 public class GetProvisionService {
@@ -17,10 +24,22 @@ public class GetProvisionService {
     private final ProvisionRepository provisionRepository;
     private final ProvisionMapper provisionMapper;
 
+    /**
+     * Método responsável por buscar todas as entidades {@code Provision}.
+     * @return {@code List<ProvisionResponse>} Uma lista de {@code ProvisionResponse}.
+     * @see ProvisionMapper#toResponse(List)
+     */
     public List<ProvisionResponse> getAllProvisions(){
         return provisionMapper.toResponse(provisionRepository.findAll());
     }
 
+    /**
+     * Método responsável por buscar uma entidade {@code Provision} por ID.
+     * @param id O ID da {@code Provision} desejada.
+     * @return ProvisionResponse A entidade encontrada no banco de dados.
+     * @throws ProvisionNotFoundException Se a {@code Provision} não for encontrada.
+     * @see ProvisionMapper#toResponse(Provision)
+     */
     public ProvisionResponse getProvisionById(Long id){
         return provisionMapper.toResponse(
             provisionRepository.findById(id).orElseThrow(() -> 

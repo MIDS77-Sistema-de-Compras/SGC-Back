f29161f13544a6e20ae46801d88cd2691dcf748a
ELIS JASPER <elis_jasper@estudante.sesisenai.org.br>
2026-05-29 20:42:50 -0300

    [API-125] docs/adiciona documentação de role

--- src/main/java/net/centroweg/gerenciamentocompras/modules/user/presentation/controller/RoleController.java
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/user/presentation/controller/RoleController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/** Endpoints relacionados aos niveis de acesso do usuário */
+
 @RestController
 @RequestMapping("/role")
 @RequiredArgsConstructor
@@ -18,31 +20,68 @@ public class RoleController {
 
     private final RoleIntrf role;
 
+    /**
+     * Cria uma nova role no sistema
+     * @param roleRequest DTO de criação de role
+     * @return status 201 - criado com sucesso, junto com DTO de reponse
+     * @see CreateRole
+     * @see RoleResponse
+     */
     @PostMapping
     public ResponseEntity<RoleResponse> createRole(@Valid @RequestBody CreateRole roleRequest){
         return ResponseEntity.status(HttpStatus.CREATED).body(role.createRole(roleRequest));
     }
 
+    /**
+     * Lista todas as roles sem filtros
+     * @return uma lista com todas as roles
+     * @see RoleResponse
+     */
     @GetMapping
     public ResponseEntity<List<RoleResponse>> listRole(){
         return ResponseEntity.ok(role.listRole());
     }
 
+    /**
+     * Retorna uma role específica com base no ID
+     * @param RoleId ID da role requisitada
+     * @return role com o ID correspondente
+     * @see RoleResponse
+     */
     @GetMapping("/RoleId/{RoleId}")
     public ResponseEntity<RoleResponse> findRoleById(@PathVariable Long RoleId){
         return ResponseEntity.ok(role.findRoleById(RoleId));
     }
 
+    /**
+     * Lista roles contendo o nome pesquisado
+     * @param RoleName nome pesquisado
+     * @return uma lista com todos os nomes correspondentes
+     * @see RoleResponse
+     */
     @GetMapping("/RoleName/{RoleName}")
     public ResponseEntity<List<RoleResponse>> findRoleByName(@PathVariable String RoleName){
         return ResponseEntity.ok(role.findRoleByName(RoleName));
     }
 
+    /**
+     * Atualiza uma role específica no sistema
+     * @param roleRequest novas informações para realizar a alteração
+     * @param RoleId ID da role a ser modificada
+     * @return status 200 com o objeto modificado
+     * @see RoleResponse
+     * @see CreateRole
+     */
     @PutMapping("/RoleId/{RoleId}")
     public ResponseEntity<RoleResponse> updateRole(@Valid @RequestBody CreateRole roleRequest, @PathVariable Long RoleId){
         return ResponseEntity.ok(role.updateRole(RoleId, roleRequest));
     }
 
+    /**
+     * Exclui role específica do sistema
+     * @param RoleId ID da role a ser excluída
+     * @return status 204
+     */
     @DeleteMapping("/RoleId/{RoleId}")
     public ResponseEntity<Void> deleteRole(@PathVariable Long RoleId){
         role.deleteRole(RoleId);
--- src/main/java/net/centroweg/gerenciamentocompras/modules/user/service/usecases/serviceimplm/role/UpdateRoleImpl.java
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/user/service/usecases/serviceimplm/role/UpdateRoleImpl.java
@@ -9,6 +9,8 @@ import net.centroweg.gerenciamentocompras.modules.user.presentation.dto.response
 import net.centroweg.gerenciamentocompras.modules.user.service.mapper.RoleMapper;
 import org.springframework.stereotype.Service;
 
+/** Classe de gerenciamento de atualização de role */
+
 @Service
 @RequiredArgsConstructor
 public class UpdateRoleImpl {
@@ -16,6 +18,15 @@ public class UpdateRoleImpl {
     private final RoleMapper mapper;
     private final RoleRepository repository;
 
+    /**
+     * Atualiza informações de role específica
+     * @param id ID da role para atualizações
+     * @param role DTO contendo novos dados para a role
+     * @return response DTO de role atualizada
+     * @throws RoleNotFoundException caso a role não seja encontrada
+     * @see RoleResponse
+     * @see CreateRole
+     */
     public RoleResponse updateRole(Long id, CreateRole role){
         Role roleSave = repository.findById(id)
                 .orElseThrow(() -> new RoleNotFoundException(id));

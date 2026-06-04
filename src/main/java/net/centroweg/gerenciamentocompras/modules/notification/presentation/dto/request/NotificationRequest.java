47b29b6d225b4c8b850fc8e958f9d23cd6eeda12
Viihh-77 <gabrielli_glowatski@estudante.sesisenai.org.br>
2026-06-04 14:39:15 -0300

    [API-131] feat/implementa-DTOs-para-notificacao

---
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/notification/presentation/dto/request/NotificationRequest.java
@@ -0,0 +1,12 @@
+package net.centroweg.gerenciamentocompras.modules.notification.presentation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record NotificationRequest(
+        @NotBlank String title,
+        @NotBlank String message,
+        @NotNull Long userId,
+        @NotNull Long requestId
+) {
+}

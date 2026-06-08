77f9a9dd966eb139b5eab80e2fba841698309b9e
Leandro Filipy <leandro_f_lima@estudante.sesisenai.org.br>
2026-06-08 15:53:01 -0300

    API-126 feature/implementa novo protocolo de criptografia para CPF de usuários

--- src/main/java/net/centroweg/gerenciamentocompras/modules/user/service/usecases/serviceimplm/user/CreateUserImpl.java
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/user/service/usecases/serviceimplm/user/CreateUserImpl.java
@@ -1,6 +1,7 @@
 package net.centroweg.gerenciamentocompras.modules.user.service.usecases.serviceimplm.user;
 
 import lombok.RequiredArgsConstructor;
+import net.centroweg.gerenciamentocompras.config.security.CpfHasher;
 import net.centroweg.gerenciamentocompras.modules.user.infrastructure.persistence.UserRepository;
 import net.centroweg.gerenciamentocompras.modules.user.presentation.dto.request.CreateUser;
 import net.centroweg.gerenciamentocompras.modules.user.presentation.dto.response.UserResponse;
@@ -15,12 +16,14 @@ public class CreateUserImpl {
     private final UserMapper mapper;
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
+    private final CpfHasher cpfHasher;
 
     public UserResponse createUser(CreateUser user){
         String encryptedPassword = passwordEncoder.encode(user.password());
-        String encryptedCPF = passwordEncoder.encode(user.cpf());
+        String hashedCpf = cpfHasher.hash(user.cpf(), encryptedPassword);
+
         CreateUser userWithEncryptedPassword = new CreateUser(
-                user.name(), user.email(), encryptedCPF, encryptedPassword, user.extensionNumber(), user.active(), user.nameRole()
+                user.name(), user.email(), hashedCpf, encryptedPassword, user.extensionNumber(), user.active(), user.nameRole()
         );
         return mapper.toDTO(repository.save(mapper.toEntity(userWithEncryptedPassword)));
     }

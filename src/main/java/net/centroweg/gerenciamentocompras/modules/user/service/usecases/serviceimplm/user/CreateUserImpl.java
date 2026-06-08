5db2b4d9c8724ce6a1f4756892fa2cac9fc2d4e6
Leandro Filipy de Lima <leandro_f_lima@estudante.sesisenai.org.br>
2026-06-08 16:04:17 -0300

    API-124 fix/ajusta-regras-de-negocio-de-usuário (#143)
    Co-authored-by: Hugo Deleon <hugo_paim@estudante.sesisenai.org.br>

--- src/main/java/net/centroweg/gerenciamentocompras/modules/user/service/usecases/serviceimplm/user/CreateUserImpl.java
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/user/service/usecases/serviceimplm/user/CreateUserImpl.java
@@ -1,6 +1,11 @@
 package net.centroweg.gerenciamentocompras.modules.user.service.usecases.serviceimplm.user;
 
 import lombok.RequiredArgsConstructor;
+import net.centroweg.gerenciamentocompras.modules.user.domain.entity.Role;
+import net.centroweg.gerenciamentocompras.modules.user.domain.entity.User;
+import net.centroweg.gerenciamentocompras.modules.user.domain.exception.RoleNotAllowedException;
+import net.centroweg.gerenciamentocompras.modules.user.domain.exception.RoleNotFoundException;
+import net.centroweg.gerenciamentocompras.modules.user.infrastructure.persistence.RoleRepository;
 import net.centroweg.gerenciamentocompras.modules.user.infrastructure.persistence.UserRepository;
 import net.centroweg.gerenciamentocompras.modules.user.presentation.dto.request.CreateUser;
 import net.centroweg.gerenciamentocompras.modules.user.presentation.dto.response.UserResponse;
@@ -15,13 +20,26 @@ public class CreateUserImpl {
     private final UserMapper mapper;
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
+    private final UniquenessValidator uniquenessValidator;
 
     public UserResponse createUser(CreateUser user){
+        uniquenessValidator.checkInfo(user);
+        Role role = roleRepository.findByName(user.nameRole()).orElseThrow(() -> new RoleNotFoundException());
         String encryptedPassword = passwordEncoder.encode(user.password());
         String encryptedCPF = passwordEncoder.encode(user.cpf());
         CreateUser userWithEncryptedPassword = new CreateUser(
+
                 user.name(), user.email(), encryptedCPF, encryptedPassword, user.extensionNumber(), user.active(), user.nameRole()
         );
-        return mapper.toDTO(repository.save(mapper.toEntity(userWithEncryptedPassword)));
+
+        if(userWithEncryptedPassword.nameRole().equals("ADMIN")){
+            throw new RoleNotAllowedException();
+        }
+        User newUser = mapper.toEntity(userWithEncryptedPassword);
+
+        newUser.setRole(role);
+
+        return mapper.toDTO(repository.save(newUser));
     }
 }

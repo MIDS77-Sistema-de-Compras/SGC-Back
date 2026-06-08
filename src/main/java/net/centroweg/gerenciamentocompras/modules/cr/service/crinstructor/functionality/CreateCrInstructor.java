9a444032a4c376c889d0fc8587c9d60da616fab6
gabrielEFagundes <gabriel_e_fagundes@estudante.sesisenai.org.br>
2026-06-08 18:11:12 -0300

    [API-130] fix: Alterando exceptions que não faziam sentido

--- src/main/java/net/centroweg/gerenciamentocompras/modules/cr/service/crinstructor/functionality/CreateCrInstructor.java
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/cr/service/crinstructor/functionality/CreateCrInstructor.java
@@ -4,13 +4,14 @@ import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import net.centroweg.gerenciamentocompras.modules.cr.domain.CrBranch;
-import net.centroweg.gerenciamentocompras.modules.cr.domain.exception.CrInstructorNotFoundException;
+import net.centroweg.gerenciamentocompras.modules.cr.domain.exception.CrBranchNotFoundException;
 import net.centroweg.gerenciamentocompras.modules.cr.infrastructure.persistence.CrBranchRepository;
 import net.centroweg.gerenciamentocompras.modules.cr.infrastructure.persistence.CrInstructorRepository;
 import net.centroweg.gerenciamentocompras.modules.cr.presentation.dto.request.CrInstructorRequest;
 import net.centroweg.gerenciamentocompras.modules.cr.presentation.dto.response.CrInstructorResponse;
 import net.centroweg.gerenciamentocompras.modules.cr.service.mapper.CrInstructorMapper;
 import net.centroweg.gerenciamentocompras.modules.user.domain.entity.User;
+import net.centroweg.gerenciamentocompras.modules.user.domain.exception.UserNotFoundException;
 import net.centroweg.gerenciamentocompras.modules.user.infrastructure.persistence.UserRepository;
 
 @Service
@@ -24,8 +25,8 @@ public class CreateCrInstructor {
     private final CrBranchRepository crBranchRepository;
 
     public CrInstructorResponse addCrInstructor(CrInstructorRequest request){
-        User user = userRepository.findById(request.instructorId()).orElseThrow(() -> new CrInstructorNotFoundException("Supervisor não encontrado."));
-        CrBranch crBranch = crBranchRepository.findById(request.crBranchId()).orElseThrow(() -> new CrInstructorNotFoundException("Filial da CR não encontrada."));
+        User user = userRepository.findById(request.instructorId()).orElseThrow(() -> new UserNotFoundException(request.instructorId()));
+        CrBranch crBranch = crBranchRepository.findById(request.crBranchId()).orElseThrow(() -> new CrBranchNotFoundException(request.crBranchId()));
 
         return crInstructorMapper.toResponse(
             crInstructorRepository.save(

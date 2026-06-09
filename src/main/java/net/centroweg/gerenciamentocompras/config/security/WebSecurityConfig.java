523e9b2a3c9644eaeb501e08defbd78c37bbccce
HUGO DELEON GIMINIANI DE SOUZA PAIM <hugo_paim@estudante.sesisenai.org.br>
2026-06-09 18:44:57 -0300

    API-106 feat/implementa indicação de rotas para permissões de acesso

--- src/main/java/net/centroweg/gerenciamentocompras/config/security/WebSecurityConfig.java
+++ src/main/java/net/centroweg/gerenciamentocompras/config/security/WebSecurityConfig.java
@@ -2,10 +2,12 @@ package net.centroweg.gerenciamentocompras.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,8 +27,11 @@ public class WebSecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorizeRequests -> {
-                    authorizeRequests.anyRequest().permitAll();
+                    authorizeRequests
+                            .requestMatchers(HttpMethod.POST, "/users", "/auth/login").permitAll()
+                            .anyRequest().authenticated();
                 })
+                .sessionManagement( session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
 

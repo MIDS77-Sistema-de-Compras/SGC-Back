75089f0ee74085fd7e2795914116a23519db365d
leandro <leandro_f_lima@estudante.sesisenai.org.br>
2026-06-10 21:47:18 -0300

    API-183 feature/implementa o método upload profile picture

--- src/main/java/net/centroweg/gerenciamentocompras/shared/claudinary/ClaudinaryService.java
+++ src/main/java/net/centroweg/gerenciamentocompras/shared/claudinary/ClaudinaryService.java
@@ -3,11 +3,13 @@ package net.centroweg.gerenciamentocompras.shared.claudinary;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Map;
 
+@Service
 @RequiredArgsConstructor
 public class ClaudinaryService {
 

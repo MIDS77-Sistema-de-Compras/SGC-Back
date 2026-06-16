3ae122a940fd353533e114be774ecce7f8d5d4aa
Leandro Filipy de Lima <leandro_f_lima@estudante.sesisenai.org.br>
2026-06-16 19:02:14 -0300

    chore(docs): atualizar documentação Swagger (API-191) (#195)
    * API-191 chore/atualiza o swagger
    
    * API-205 chore/configura mais de um ambiente para properties
    
    ---------
    
    Co-authored-by: HUGO DELEON GIMINIANI DE SOUZA PAIM <hugo_paim@estudante.sesisenai.org.br>

--- src/main/java/net/centroweg/gerenciamentocompras/modules/cr/presentation/controller/SectorController.java
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/cr/presentation/controller/SectorController.java
@@ -1,5 +1,7 @@
 package net.centroweg.gerenciamentocompras.modules.cr.presentation.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import net.centroweg.gerenciamentocompras.modules.cr.presentation.dto.request.SectorRequest;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "ENDPOINTS da entidade SECTOR")
 @RestController
 @RequestMapping("/sector")
 @RequiredArgsConstructor
@@ -19,36 +22,43 @@ public class SectorController {
 
     private final SectorService service;
 
+    @Operation(description = "ENDPOINT responsável pela criação de Sector")
     @PostMapping
     public ResponseEntity<SectorSimpleResponse> createSector(@Valid @RequestBody SectorRequest sector){
         return ResponseEntity.status(HttpStatus.CREATED).body(service.createSector(sector));
     }
 
+    @Operation(description = "ENDPOINT responsável pela listagem simplificada de todos Sector")
     @GetMapping("/simple")
     public ResponseEntity<List<SectorSimpleResponse>> findAllSectorSimple(){
         return ResponseEntity.ok(service.findAllSectorSimple());
     }
 
+    @Operation(description = "ENDPOINT responsável pela listagem completa de todos Sector")
     @GetMapping("/compound")
     public ResponseEntity<List<SectorCompoundResponse>> findAllSectorCompound(){
         return ResponseEntity.ok(service.findAllSectorCompound());
     }
 
+    @Operation(description = "ENDPOINT responsável pela listagem simplificada de Sector por id")
     @GetMapping("/simple/{id}")
     public ResponseEntity<SectorSimpleResponse> findSectorByIdSimple(@PathVariable Long id){
         return ResponseEntity.ok(service.findSectorByIdSimple(id));
     }
 
+    @Operation(description = "ENDPOINT responsável pela listagem completa de Sector por id")
     @GetMapping("/compound/{id}")
     public ResponseEntity<SectorCompoundResponse> findSectorByIdCompound(@PathVariable Long id){
         return ResponseEntity.ok(service.findSectorByIdCompound(id));
     }
 
+    @Operation(description = "ENDPOINT responsável pela atualização de Sector")
     @PutMapping("/{id}")
     public ResponseEntity<SectorSimpleResponse> updateSector(@Valid @RequestBody SectorRequest sector, @PathVariable Long id){
         return ResponseEntity.ok(service.updateSector(id, sector));
     }
 
+    @Operation(description = "ENDPOINT responsável pelo delete de Sector")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteSector(@PathVariable Long id){
         service.deleteSector(id);
--- src/main/java/net/centroweg/gerenciamentocompras/modules/user/presentation/controller/UserController.java
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/user/presentation/controller/UserController.java
@@ -1,5 +1,7 @@
 package net.centroweg.gerenciamentocompras.modules.user.presentation.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import net.centroweg.gerenciamentocompras.modules.user.presentation.dto.request.CreateUser;
@@ -13,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
+@Tag(name = "ENDPOINTS da entidade USER")
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
@@ -20,31 +23,37 @@ public class UserController {
 
     private final UserIntrf user;
 
+    @Operation(description = "ENDPOINT responsável pela criação de User")
     @PostMapping
     public ResponseEntity<UserResponse> createUser(@Valid @RequestBody CreateUser userRequest){
         return ResponseEntity.status(HttpStatus.CREATED).body(user.createUser(userRequest));
     }
 
+    @Operation(description = "ENDPOINT responsável pela listagem de todos User")
     @GetMapping
     public ResponseEntity<List<UserResponse>> listUser(){
         return ResponseEntity.ok(user.listUser());
     }
 
+    @Operation(description = "ENDPOINT responsável pela listagem de User por id")
     @GetMapping("/userId/{userId}")
     public ResponseEntity<UserResponse> findUserById(@PathVariable Long userId){
         return ResponseEntity.ok(user.findUserById(userId));
     }
 
+    @Operation(description = "ENDPOINT responsável pela listagem de User por nome")
     @GetMapping("/userName/{userName}")
     public ResponseEntity<List<UserResponse>> findUserByName(@PathVariable String userName){
         return ResponseEntity.ok(user.findUserByName(userName));
     }
 
+    @Operation(description = "ENDPOINT responsável pela atualização de User")
     @PutMapping("/userId/{userId}")
     public ResponseEntity<UserResponse> updateUser(@Valid @RequestBody CreateUser userRequest, @PathVariable Long userId){
         return ResponseEntity.ok(user.updateUserAll(userId, userRequest));
     }
 
+    @Operation(description = "ENDPOINT responsável pelo delete de User")
     @DeleteMapping("/userId/{userId}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long userId){
         user.deleteUser(userId);

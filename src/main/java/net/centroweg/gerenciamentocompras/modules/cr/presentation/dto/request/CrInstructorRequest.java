47b5869ca18f8df9167cbf23404a8bd5f073aeaf
gabrielEFagundes <gabriel_e_fagundes@estudante.sesisenai.org.br>
2026-06-26 20:51:04 -0300

    [API-276] fix: Ajusta mensagem com interpretação errônea de supervisor para professor

--- src/main/java/net/centroweg/gerenciamentocompras/modules/cr/presentation/dto/request/CrInstructorRequest.java
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/cr/presentation/dto/request/CrInstructorRequest.java
@@ -6,8 +6,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record CrInstructorRequest(
-    @NotNull(message = "O ID do supervisor não deve ser nulo.")
-    List<@Positive(message = "O ID do supervisor não pode ser menor que 1.") Long> instructorIds,
+    @NotNull(message = "O ID do professor não deve ser nulo.")
+    List<@Positive(message = "O ID do professor não pode ser menor que 1.") Long> instructorIds,
 
     @NotNull(message = "O ID da filial não deve ser nulo.")
     @Positive(message = "O ID da filial não pode ser menor que 1.")

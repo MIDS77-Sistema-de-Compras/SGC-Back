9723238ec358f7bf9a4cb9ac958ef8f22f8d6449
HUGO DELEON GIMINIANI DE SOUZA PAIM <hugo_paim@estudante.sesisenai.org.br>
2026-06-30 21:44:25 -0300

    API-284 fix/implementa o que restava no controller e services

--- src/main/java/net/centroweg/gerenciamentocompras/shared/audit/service/usecase/impl/AuditLogServiceImpl.java
+++ src/main/java/net/centroweg/gerenciamentocompras/shared/audit/service/usecase/impl/AuditLogServiceImpl.java
@@ -1,6 +1,7 @@
 package net.centroweg.gerenciamentocompras.shared.audit.service.usecase.impl;
 
 import lombok.RequiredArgsConstructor;
+import net.centroweg.gerenciamentocompras.shared.audit.presentation.dto.request.AuditLogFilterRequest;
 import net.centroweg.gerenciamentocompras.shared.audit.presentation.dto.response.AuditLogDTOResponse;
 import net.centroweg.gerenciamentocompras.shared.audit.service.usecase.interfaces.AuditLogService;
 import org.springframework.stereotype.Service;
@@ -14,7 +15,7 @@ public class AuditLogServiceImpl implements AuditLogService {
     private final ListAuditLogAll listAuditLogAll;
 
     @Override
-    public List<AuditLogDTOResponse> findAll() {
-        return listAuditLogAll.findAll();
+    public List<AuditLogDTOResponse> findAll(AuditLogFilterRequest filter) {
+        return listAuditLogAll.findAll(filter);
     }
 }

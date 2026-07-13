d011d51ef7efbed8920de3b9142e65f4e420d45b
gabrielEFagundes <gabriel_e_fagundes@estudante.sesisenai.org.br>
2026-07-13 15:27:33 -0300

    [API-331] chore: add validacao nos dtos de request

--- src/main/java/net/centroweg/gerenciamentocompras/modules/request/presentation/dto/request/RequestProductItemRequest.java
+++ src/main/java/net/centroweg/gerenciamentocompras/modules/request/presentation/dto/request/RequestProductItemRequest.java
@@ -1,20 +1,23 @@
 package net.centroweg.gerenciamentocompras.modules.request.presentation.dto.request;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record RequestProductItemRequest(
-        @NotBlank(message = "O nome do produto e obrigatorio.")
+        @NotBlank(message = "O nome do produto é obrigatorio.")
         String productName,
 
-        @NotBlank(message = "A unidade de medida e obrigatoria.")
+        @NotBlank(message = "A unidade de medida é obrigatoria.")
         String measurementUnit,
 
-        @NotNull(message = "A quantidade e obrigatoria.")
+        @NotNull(message = "A quantidade é obrigatoria.")
         @Positive(message = "A quantidade deve ser maior que zero.")
         Double quantity,
 
+        @NotBlank(message = "As informações adicionais do produto não podem estar em branco.")
+        @Max(value=255, message = "Informações adicionais excedem o limite máximo permitido (255 caractéres)")
         String additionalInformations
 ) {
 }
